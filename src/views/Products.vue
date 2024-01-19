cb08a92a714cc66c98cebc01d98ee6e9392c4b40
xxXWarMachineRoXxx <rijul123.sharma@outlook.com>
2024-01-19 18:20:21 +0530

    triggering azure build pipeline

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -10,22 +10,18 @@ import { defineComponent } from 'vue';
 import Navbar from '../components/Organisms/Navbar/Navbar.vue';
 import Breadcrumbs from '../components/Molecules/Breadcrumbs/Breadcrumbs.vue';
 import FilterSideBar from '../components/Organisms/FilterSideBar/FilterSideBar.vue';
-interface Product {
-  id: number;
-  name: string;
-}
 
 export default defineComponent({
-    data() {
-        return {
-            products: [
-                { id: 1, name: 'Product 1' },
-                { id: 2, name: 'Product 2' },
-                { id: 3, name: 'Product 3' },
-                // Add more products here as needed
-            ] as Product[]
-        };
-    },
+    // data() {
+    //     return {
+    //         products: [
+    //             { id: 1, name: 'Product 1' },
+    //             { id: 2, name: 'Product 2' },
+    //             { id: 3, name: 'Product 3' },
+    //             // Add more products here as needed
+    //         ] as Product[]
+    //     };
+    // },
     components: { Navbar,Breadcrumbs,FilterSideBar }
 });
 </script>
