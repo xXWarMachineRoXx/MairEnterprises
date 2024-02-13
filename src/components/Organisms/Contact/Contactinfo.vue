ac54d45330e1e682052ae73a2ccd532bb6b964f0
Aditya Bansal <bansaladitya@gmail.com>
2024-02-14 02:53:00 +0530

    Contact us

--- src/components/Organisms/Contact/Contactinfo.vue
+++ src/components/Organisms/Contact/Contactinfo.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="flex flex-wrap justify-center items-center">
-        <div class="flex bg-primary-50 p-5 grow basis-1/2 border-secondary-200 border-2 !m-5 max-w-[70%]">
+        <div class="flex bg-primary-50 p-5 grow basis-1/2 border-secondary-700 border-2 !m-5 max-w-[70%]">
 
-      <SfIconLocationOnFilled size="2xl" class="" />
-        <p class="leading-4 justify-normal max-w-96 font-semibold pl-2.5">First Floor, Office No. 5A, Block-A, Omaxe Gurgaon Mall,Sohna Road, Sector 49, Gurgaon, Gurugram-122018, Haryana, India</p>
+      <SfIconLocationOnFilled size="2xl" class="text-primary-700" />
+        <p class="leading-4 justify-normal max-w-96 font-semibold pl-2.5 text-primary-900">First Floor, Office No. 5A, Block-A, Omaxe Gurgaon Mall,Sohna Road, Sector 49, Gurgaon, Gurugram-122018, Haryana, India</p>
         </div>
-        <div class="flex bg-primary-50 p-5 grow basis-1/2 border-secondary-200 border-2 !m-5 items-center  text-xl max-w-[70%]">
-        <div class="flex items-center basis-1/2 border-r-2 border-secondary-200">
-      <SfIconCall size="2xl" class="" />
-        <p class="leading-4 justify-normal max-w-72 font-semibold pl-2.5">12345678901</p>
+        <div class="flex bg-primary-50 p-5 grow basis-1/2 border-secondary-700 border-2 !m-5 items-center  text-xl max-w-[70%]">
+        <div class="flex items-center basis-1/2 border-r-2 border-secondary-700">
+      <SfIconCall size="2xl" class="text-primary-700" />
+        <p class="leading-4 justify-normal max-w-72 font-semibold pl-2.5 text-primary-900">12345678901</p>
         </div>
         <div class="flex items-center basis-1/2 pl-5">
-            <SfIconPerson size="2xl" class="" />
-            <p class="leading-4 justify-normal max-w-72 font-semibold pl-2.5">Rajendra Mair (Owner)</p>
-</div>
+            <SfIconPerson size="2xl" class="text-primary-700" />
+            <p class="leading-4 justify-normal max-w-72 font-semibold pl-2.5 text-primary-900">Rajendra Mair (Owner)</p>
+        </div>
         </div>
     </div>
   </template>
