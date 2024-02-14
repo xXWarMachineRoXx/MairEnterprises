e08951131d3e9b0a7d44a35580ee7f87316735d0
xxXWarMachineRoXxx <rijul123.sharma@outlook.com>
2024-02-14 16:42:58 +0530

    topbar phonelink fixed and footer populated with relevant content

--- src/components/Organisms/Topbar/TopBar.vue
+++ src/components/Organisms/Topbar/TopBar.vue
@@ -1,10 +1,13 @@
 <template>
-    <SfButton class="w-full rounded-none !bg-primary-300 !text-primary-800">  <Component :is="SfIconCall" /> Call for best deals <a href="tel:+917942969133">+91 794 296 9133</a> </SfButton>
+    <SfButton @click="call" class="w-full rounded-none !bg-primary-300 !text-primary-800">  <Component :is="SfIconCall" /> Call us at: +91 794 296 9133 </SfButton>
 </template>
   
 <script lang="ts" setup>
 import { SfButton } from '@storefront-ui/vue';
 import { SfIconCall } from '@storefront-ui/vue';
 
+function call() {
+  location.href = "tel:+917942969133";
+}
 
 </script>
